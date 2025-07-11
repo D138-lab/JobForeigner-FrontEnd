eba7031efba29e248ebae477df3cd1edbcd00f5e
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-11 09:00:41 +0900

    feat: 연봉 정보 api 데이터로 갱신
    #70

--- src/pages/companies/subPage/SalaryInfo.tsx
+++ src/pages/companies/subPage/SalaryInfo.tsx
@@ -1,19 +1,24 @@
-import SalaryBox from "@/components/companies/SalaryBox";
-import styles from "./salaryInfo.module.scss";
-import AnnualHiresChart from "@/components/companies/AnnualHiresChart";
+import AnnualHiresChart from '@/components/companies/AnnualHiresChart';
+import SalaryBox from '@/components/companies/SalaryBox';
+import styles from './salaryInfo.module.scss';
 
-const SalaryInfo = () => {
-	return (
-		<div className={styles.container}>
-			<div className={styles.salaryBoxContainer}>
-				<SalaryBox titleText="전체 연봉" value={5978} scale="만원" />
-				<SalaryBox titleText="월 실수령액" value={4197210} scale="원" />
-			</div>
-			<div className={styles.chartContainer}>
-				<AnnualHiresChart />
-			</div>
-		</div>
-	);
+interface SalaryInfoProps {
+  averageSalary: number;
+  monthlySalary: number;
+}
+
+const SalaryInfo = ({ averageSalary, monthlySalary }: SalaryInfoProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.salaryBoxContainer}>
+        <SalaryBox titleText='전체 연봉' value={averageSalary} scale='만원' />
+        <SalaryBox titleText='월 실수령액' value={monthlySalary} scale='원' />
+      </div>
+      <div className={styles.chartContainer}>
+        <AnnualHiresChart />
+      </div>
+    </div>
+  );
 };
 
 export default SalaryInfo;
