f134bb35081efec23cc8625610eb342e19eef2d8
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-12 14:09:09 +0900

    feat: 로그인 api 연결
    #65

--- src/lib/apis/mutations/usePostSignin.ts
+++ src/lib/apis/mutations/usePostSignin.ts
@@ -1,10 +1,24 @@
+import { CustomAxiosRequestConfig, instance } from '@/lib/fetcher';
+
 import { END_POINTS } from '@/lib/constants/routes';
-import { fetcher } from '@/lib/fetcher';
+import { LOCAL_STORAGE } from '@/lib/constants';
 import { LoginValues } from '@/lib/schemas/loginSchema';
 import { useMutation } from '@tanstack/react-query';
 
 const postSignin = async (body: LoginValues) => {
-  return fetcher.post<LoginValues>(END_POINTS.SIGN_IN, body);
+  const config: CustomAxiosRequestConfig = {
+    skipAuth: true,
+    withCredentials: true,
+  };
+
+  const response = await instance.post(END_POINTS.SIGN_IN, body, config);
+
+  const accessToken = response.headers['authorization'];
+  if (accessToken) {
+    localStorage.setItem(LOCAL_STORAGE.ACCESS_TOKEN, accessToken);
+  }
+
+  return response.data;
 };
 
 const usePostSignin = () =>
@@ -13,12 +27,10 @@ const usePostSignin = () =>
     mutationKey: ['signin'],
     onSuccess: data => {
       console.log('Post successful:', data);
-
       return data;
     },
     onError: error => {
       console.error('Post failed:', error);
-
       return error;
     },
   });
