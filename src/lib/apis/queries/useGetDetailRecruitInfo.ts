031213082c74178f13e5082ac626be59ef9bebc4
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-14 10:57:38 +0900

    feat: 채용 공고 상세 페이지 api 훅 구현
    #72

---
+++ src/lib/apis/queries/useGetDetailRecruitInfo.ts
@@ -0,0 +1,31 @@
+import { fetcher } from '@/lib/fetcher';
+import { useQuery } from '@tanstack/react-query';
+
+interface GetDetailRecruitResponse {
+  id: number;
+  title: string;
+  companyName: string;
+  description: string;
+  location: string;
+  employmentType: string;
+  salary: string;
+  career: string;
+  published: string;
+  grade: string;
+  isScrapped: boolean;
+  expiryAt: Date;
+}
+
+const useGetDetailRecruitInfo = (id: string) => {
+  return {
+    ...useQuery({
+      queryKey: ['getDetailRecruitInfo', id],
+      queryFn: () =>
+        fetcher.get<{ success: boolean; data: GetDetailRecruitResponse }>(
+          `https://jb.d138.patulus.com:8080/api/v1/job-posts/${id}`,
+        ),
+    }),
+  };
+};
+
+export default useGetDetailRecruitInfo;
