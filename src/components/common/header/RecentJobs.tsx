1a8e1a3eb1e05b171d64b8b96a9f2a89988cd42e
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-29 09:38:15 +0900

    feat: 최근 본 공고 조회 UI 퍼블리싱
    #86

--- src/components/common/header/RecentJobs.tsx
+++ src/components/common/header/RecentJobs.tsx
@@ -1,14 +1,16 @@
+import { History } from 'lucide-react';
 import RecentModal from './RecentModal';
-import { View } from 'lucide-react';
 import styles from './recentJobs.module.scss';
+import useGetRecentJobs from '@/lib/apis/queries/useGetRecentJobs';
 import { useState } from 'react';
 
 const RecentJobs = () => {
   const [isModalOn, setIsModalOn] = useState<boolean>(false);
+  const { data } = useGetRecentJobs();
   return (
     <div className={styles.container}>
-      <View onClick={() => setIsModalOn(!isModalOn)} />
-      {isModalOn && <RecentModal />}
+      <History onClick={() => setIsModalOn(!isModalOn)} />
+      {isModalOn && <RecentModal data={data?.data ?? []} />}
     </div>
   );
 };
--- src/components/common/header/RecentModal.tsx
+++ src/components/common/header/RecentModal.tsx
@@ -1,9 +1,29 @@
+import { GetRecentJobsResponse } from '@/lib/apis/queries/useGetRecentJobs';
+import { History } from 'lucide-react';
 import styles from './recentModal.module.scss';
 
-const RecentModal = () => {
+type Props = {
+  data: GetRecentJobsResponse[];
+};
+
+const RecentModal = ({ data }: Props) => {
   return (
     <div className={styles.container}>
-      <div>hh</div>
+      <div className={styles.labelBox}>
+        <History size={15} />
+        <span>최근 본 공고</span>
+      </div>
+      <div className={styles.recentList}>
+        {data &&
+          data.map(ele => (
+            <div className={styles.recentBox}>{ele.companyName}</div>
+          ))}
+        {data.length === 0 ? (
+          <div className={styles.recentBox}>최근 본 공고가 없습니다.</div>
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   );
 };
