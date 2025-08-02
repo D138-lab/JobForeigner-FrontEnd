0e74b088e1f59a5f87c09fdf3eb325c065050b59
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-02 15:36:57 +0900

    feat: swiper 라이브러리 적용
    #97

--- src/components/main/LongAdvertiseBanner.tsx
+++ src/components/main/LongAdvertiseBanner.tsx
@@ -1,4 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+
+import { Autoplay, EffectCoverflow } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import styles from './longAdvertisementBanner.module.scss';
 
@@ -12,36 +16,31 @@ type Props = {
 };
 
 const LongAdvertisementBanner = ({ data }: Props) => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-  const groupSize = 1;
-  const totalGroups = Math.ceil(data.length / groupSize);
-
-  useEffect(() => {
-    timeoutRef.current = setTimeout(() => {
-      setCurrentIndex(prev => (prev + 1) % totalGroups);
-    }, 3000);
-
-    return () => {
-      if (timeoutRef.current) clearTimeout(timeoutRef.current);
-    };
-  }, [currentIndex, totalGroups]);
-
-  const getVisibleAds = () => {
-    const start = currentIndex * groupSize;
-    return data.slice(start, start + groupSize);
-  };
-
   return (
     <div className={styles.container}>
-      <div className={styles.slider}>
-        {getVisibleAds().map((d, idx) => (
-          <div key={idx} className={styles.adBox}>
+      <Swiper
+        modules={[EffectCoverflow, Autoplay]}
+        effect='coverflow'
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView='auto'
+        loop={true}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 1.5,
+          slideShadows: true,
+        }}
+        className={styles.slider}
+      >
+        {data.map((d, idx) => (
+          <SwiperSlide key={idx} className={styles.adBox}>
             <img src={d.imgUrl} alt={d.adName} />
-          </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 };
