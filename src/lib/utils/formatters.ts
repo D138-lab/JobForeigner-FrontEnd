a3d166d32818aaa38539f9de536658805ac5b972
dswvgw6412 <dswvgw6412@naver.com>
2025-08-25 10:11:42 +0900

    refactor: 전화번호 포매터 리팩토링

--- src/lib/utils/formatters.ts
+++ src/lib/utils/formatters.ts
@@ -1,8 +1,16 @@
+const NUMBER_ONLY_REGEX = /[^0-9]/g;
+const PHONE_NUMBER_FORMAT_REGEX =
+  /(^02.{0}|^01.{1}|[0-9]{3,4})([0-9]{3,4})([0-9]{4})/;
+
+const formatToHyphenatedNumber = (
+  _: string,
+  prefix: string,
+  middle: string,
+  suffix: string,
+) => `${prefix}-${middle}-${suffix}`;
+
 export const formatPhoneNumber = (value: string) => {
   return value
-    .replace(/[^0-9]/g, '')
-    .replace(
-      /(^02.{0}|^01.{1}|[0-9]{3,4})([0-9]{3,4})([0-9]{4})/,
-      (_, p1, p2, p3) => `${p1}-${p2}-${p3}`,
-    );
+    .replace(NUMBER_ONLY_REGEX, '')
+    .replace(PHONE_NUMBER_FORMAT_REGEX, formatToHyphenatedNumber);
 };
