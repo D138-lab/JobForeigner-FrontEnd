383ace9361603204032af5165f1ad6741e1deca1
dswvgw6412 <dswvgw6412@naver.com>
2025-08-25 17:33:49 +0900

    refactor: slice 패턴으로 관심사 분리

--- src/lib/stores/useAuthStore.ts
+++ src/lib/stores/useAuthStore.ts
@@ -1,40 +1,56 @@
-import { create } from 'zustand';
+import { create, StateCreator } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 type UserType = 'NONE' | 'FOREIGNER' | 'COMPANY' | 'ADMIN';
+type AddressType = {
+  address: string;
+  detailAddress: string;
+  zipcode: string;
+};
 
-interface AuthStateValues {
+// Login Slice 관련 타입
+interface LoginState {
   isLoggedIn: boolean;
+}
+
+interface LoginActions {
+  login: () => void;
+  logout: () => void;
+}
+
+type LoginSlice = LoginState & LoginActions;
+
+// User Info Slice 관련 타입
+interface UserInfoState {
   name: string;
   type: UserType;
   phoneNumber: string;
   email: string;
   profileImageUrl: string;
-  address: {
-    address: string;
-    detailAddress: string;
-    zipcode: string;
-  };
+  address: AddressType;
 }
 
-interface AuthStateActions {
-  login: () => void;
-  logout: () => void;
+interface UserInfoActions {
   setName: (name: string) => void;
   setType: (type: UserType) => void;
   setPhoneNumber: (phoneNumber: string) => void;
   setEmail: (email: string) => void;
   setProfileImageUrl: (url: string) => void;
-  setAddress: (addressObj: {
-    address: string;
-    detailAddress: string;
-    zipcode: string;
-  }) => void;
+  setAddress: (addressObj: AddressType) => void;
 }
 
-export const useAuthStore = create<AuthStateValues & AuthStateActions>()(
-  persist(
-    set => ({
+type UserInfoSlice = UserInfoState & UserInfoActions;
+
+interface AuthStore extends LoginSlice, UserInfoSlice {}
+
+const createLoginSlice: StateCreator<AuthStore, [], [], LoginSlice> = set => ({
+  isLoggedIn: false,
+  login: () =>
+    set(() => ({
+      isLoggedIn: true,
+    })),
+  logout: () =>
+    set(() => ({
       isLoggedIn: false,
       name: '',
       type: 'NONE',
@@ -46,32 +62,41 @@ export const useAuthStore = create<AuthStateValues & AuthStateActions>()(
         detailAddress: '',
         zipcode: '',
       },
+    })),
+});
 
-      login: () => set({ isLoggedIn: true }),
-      logout: () =>
-        set({
-          isLoggedIn: false,
-          name: '',
-          type: 'NONE',
-          phoneNumber: '',
-          email: '',
-          profileImageUrl: '',
-          address: {
-            address: '',
-            detailAddress: '',
-            zipcode: '',
-          },
-        }),
+const createUserInfoSlice: StateCreator<
+  AuthStore,
+  [],
+  [],
+  UserInfoSlice
+> = set => ({
+  name: '',
+  type: 'NONE',
+  phoneNumber: '',
+  email: '',
+  profileImageUrl: '',
+  address: {
+    address: '',
+    detailAddress: '',
+    zipcode: '',
+  },
+  setName: name => {
+    console.log('[setName]', name);
+    set({ name });
+  },
+  setType: type => set({ type }),
+  setPhoneNumber: phoneNumber => set({ phoneNumber }),
+  setEmail: email => set({ email }),
+  setProfileImageUrl: url => set({ profileImageUrl: url }),
+  setAddress: addressObj => set({ address: addressObj }),
+});
 
-      setName: name => {
-        console.log('[setName]', name);
-        set({ name });
-      },
-      setType: type => set({ type }),
-      setPhoneNumber: phoneNumber => set({ phoneNumber }),
-      setEmail: email => set({ email }),
-      setProfileImageUrl: url => set({ profileImageUrl: url }),
-      setAddress: addressObj => set({ address: addressObj }),
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (set, get, store) => ({
+      ...createLoginSlice(set, get, store),
+      ...createUserInfoSlice(set, get, store),
     }),
     {
       name: 'my-info',
