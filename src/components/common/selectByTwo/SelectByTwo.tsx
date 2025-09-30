7b5b1455765d41c9c63abeee9340e4a13d23b217
minwoo1119 <minwoo1119@naver.com>
2025-09-30 20:51:40 +0900

    feat: selectByTwo 컴포넌트 구현
    #100

---
+++ src/components/common/selectByTwo/SelectByTwo.tsx
@@ -0,0 +1,50 @@
+import { Dispatch, SetStateAction } from 'react';
+import { List, Map } from 'lucide-react';
+
+import styles from './selectByTwo.module.scss';
+
+interface Props {
+  firstIcon?: string;
+  secondIcon?: string;
+  firstText: string;
+  secondText: string;
+  selected: string;
+  setSelected: Dispatch<SetStateAction<string>>;
+}
+
+const getIcon = (icon?: string) => {
+  if (icon === 'map') return <Map size={15} />;
+  if (icon === 'list') return <List size={15} />;
+};
+
+export const SelectByTwo = ({
+  firstIcon,
+  secondIcon,
+  firstText,
+  secondText,
+  selected,
+  setSelected,
+}: Props) => {
+  return (
+    <div className={styles.container}>
+      <div
+        className={`${styles.firstItem} ${
+          selected === firstText ? styles.selected : ''
+        }`}
+        onClick={() => setSelected(firstText)}
+      >
+        {getIcon(firstIcon)}
+        <span>{firstText}</span>
+      </div>
+      <div
+        className={`${styles.secondItem} ${
+          selected === secondText ? styles.selected : ''
+        }`}
+        onClick={() => setSelected(secondText)}
+      >
+        {getIcon(secondIcon)}
+        <span>{secondText}</span>
+      </div>
+    </div>
+  );
+};
