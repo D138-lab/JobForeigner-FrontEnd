6fd9223a57d6e58be3ec4388ae57892452c2f92b
minwoo1119 <minwoo1119@naver.com>
2026-02-15 14:24:14 +0900

    fix: isLiked 및 onLikeClick, onCommentClick 추가 #109

--- src/components/community/LikeAndComments.tsx
+++ src/components/community/LikeAndComments.tsx
@@ -5,20 +5,30 @@ import styles from './likeAndComments.module.scss';
 interface LikeAndCommentsProps {
   numOfLike: number;
   numOfComment: number;
+  isLiked: boolean;
+  onLikeClick: () => void;
+  onCommentClick: () => void;
 }
 
 export const LikeAndComments = ({
   numOfLike,
   numOfComment,
+  isLiked,
+  onLikeClick,
+  onCommentClick,
 }: LikeAndCommentsProps) => {
   return (
     <div className={styles.container}>
-      <div className={styles.iconAndText}>
-        <Heart size={15} />
+      <div className={styles.iconAndText} onClick={onLikeClick}>
+        <Heart
+          size={20}
+          fill={isLiked ? '#FF0000' : '#ffffff'}
+          color={isLiked ? '#FF0000' : '#000000'}
+        />
         <span>{numOfLike}</span>
       </div>
-      <div className={styles.iconAndText}>
-        <MessageCircle size={15} />
+      <div className={styles.iconAndText} onClick={onCommentClick}>
+        <MessageCircle size={20} />
         <span>{numOfComment}</span>
       </div>
     </div>
