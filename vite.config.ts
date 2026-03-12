c326a1d9d1afb8d2ac4df36941c52a76ff5b1a34
minwoo1119 <minwoo1119@naver.com>
2026-03-12 20:46:08 +0900

    feat: 디자인 토큰 정의 및 적용 #117

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "@/styles/variables" as *;',
+      },
+    },
+  },
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
